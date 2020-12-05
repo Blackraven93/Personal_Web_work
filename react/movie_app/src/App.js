2755749beaf832673dcc5b781ccdd11127414fb3
kwak se jin <Reblackraven@gmail.com>
2020-12-05 21:14:36 +0900

    Make css file

--- react/movie_app/src/App.js
+++ react/movie_app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// index에서 render를 이용해 하나의 function만 가능하므로 정의한 함수에 파편적으로 넣을 것
+function Food({ name }) {
+  return (
+    <h1>I like {name}</h1>
+  )
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name: 'kimchi',
+  },
+  {
+    id: 2,
+    name: 'Samgyeopsal',
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {foodILike.map(dish => <Food key={dish.id} name={dish.name} />)}
+    </div >
   );
 }
+// component 정의시 대문자로
 
 export default App;
